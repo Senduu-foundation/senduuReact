a13626e90e655b772005873f5c9466263c36e7f9
osemu <vincentaimiosior@example.com>
2025-07-03 15:50:23 +0100

    updates

--- senduu_frontend/src/App.js
+++ senduu_frontend/src/App.js
@@ -2,12 +2,14 @@ import Header from './components/header'
 import Content from './components/content'
 import Footer from './components/footer'
 import Hero from './components/hero'
+import Hero2 from './components/hero2'
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <Hero />
+      <Hero2 />
       <Content/>
       <Footer/>
     </div>
---
+++ senduu_frontend/src/components/hero2.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const hero2 = () => {
+  return (
+    <div className='how-it-works'>
+        <h2>
+            How it Works
+        </h2>
+        <div className='steps'>
+          <h2 className='step'>
+            Add Projects
+          </h2>
+        </div>
+    </div>
+  )
+}
+
+export default hero2
